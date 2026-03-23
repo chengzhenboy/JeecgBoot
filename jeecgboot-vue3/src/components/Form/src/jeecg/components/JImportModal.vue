5574c635e0736a50788ed06c174b448ae8dcb420
243691461@qq.com <243691461@qq.com>
2026-03-23 09:51:17 +0800

    提交

--- jeecgboot-vue3/src/components/Form/src/jeecg/components/JImportModal.vue
+++ jeecgboot-vue3/src/components/Form/src/jeecg/components/JImportModal.vue
@@ -5,7 +5,7 @@
       <div style="margin: 0 5px 5px" v-if="online">
         <span style="display: inline-block; height: 32px; line-height: 32px; vertical-align: middle">是否开启校验:</span>
         <span style="margin-left: 6px">
-          <a-switch :checked="validateStatus == 1" @change="handleChangeValidateStatus" checked-children="是" un-checked-children="否" :disabled="true" />
+          <a-switch :checked="validateStatus == 0" @change="handleChangeValidateStatus" checked-children="是" un-checked-children="否" :disabled="true" />
         </span>
       </div>
       <!--上传-->
@@ -71,7 +71,7 @@
       const uploadAction = ref('');
       const foreignKeys = ref('');
       //校验状态
-      const validateStatus = ref(1);
+      const validateStatus = ref(0);
       const getBindValue = Object.assign({}, unref(props), unref(attrs));
       //监听url
       watchEffect(() => {
@@ -89,7 +89,7 @@
 
       //校验状态切换
       function handleChangeValidateStatus(checked) {
-        validateStatus.value = !!checked ? 1 : 0;
+        validateStatus.value = !!checked ? 0 : 1;
       }
 
       //移除上传文件
